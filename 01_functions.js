f04523e486c7391f50267b81c82a995a192a5e33
Gyana Ranjan Rout <rgyana.dev@gmail.com>
2026-06-16 18:35:22 +0530

    functions

---
+++ 01_functions.js
@@ -0,0 +1,23 @@
+// Normal function declaration
+function add(a, b) {
+    return a + b;
+}
+
+let result = add(5, 3);
+console.log(result);
+
+
+// Arrow function expression
+let multiply = (x, y) => {
+    return x * y;
+}
+let result2 = multiply(39, 342);
+console.log(result2);
+
+
+// Function Expression
+let divide = function (p, q) {
+    return p / q;
+};
+let result3 = divide(10, 2);
+console.log(result3);
