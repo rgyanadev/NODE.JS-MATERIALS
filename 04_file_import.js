fa934ccacee04b0cc7acd683a7bc33f8257e6388
Gyana Ranjan Rout <rgyana.dev@gmail.com>
2026-06-20 12:18:50 +0530

    file import and export 002

--- 04_file_import.js
+++ 04_file_import.js
@@ -1,4 +1,9 @@
 const notes = require('./notes.js');
 
+// Simple variable import from notes.js file
 let age = notes.age;
 console.log("Age is: " + age);
+
+// Function import from notes.js file
+let sum = notes.addNumbers(5, 10);
+console.log("Sum is: " + sum);
--- notes.js
+++ notes.js
@@ -4,6 +4,13 @@ console.log("Notes page is loaded again");
 let age = 10;
 // now if we want to assess this in different file then we have to export this variable.
 
-module.exports = {
-    age
+function addNumbers(num1, num2) {
+    return num1 + num2;
 }
+
+module.exports = {
+    age,
+    addNumbers
+}
+
+// We can export anything like function, object, array, string, number etc. and we can access that in different file by using require() method.
